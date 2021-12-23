ccd13af2ea3eb43200affeb940bbf5ce00d0110c
tokunagadai <tokunagadai@192.168.10.109>
2021-12-23 16:44:09 +0900

    new create

---
+++ SpringBootTest202112/src/main/java/com/servesync/spring/SpringBootTest202112Application.java
@@ -0,0 +1,13 @@
+package com.servesync.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTest202112Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTest202112Application.class, args);
+	}
+
+}
---
+++ SpringBootTest202112/src/test/java/com/servesync/spring/SpringBootTest202112ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servesync.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTest202112ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
